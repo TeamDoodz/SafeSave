1efde2f8f4ddfb5530386d740ee6415c749ff2b0
TeamDoodz <unconfigured@null.spigotmc.org>
2022-08-10 19:00:18 -0500

    Better error handling

--- SafeSave/JsonSaver.cs
+++ SafeSave/JsonSaver.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using BepInEx.Configuration;
 using Newtonsoft.Json;
+using UnityEngine;
 
 namespace SafeSave {
 	public static class JsonSaver {
@@ -33,37 +34,56 @@ namespace SafeSave {
 		}
 
 		public static RankData LoadRankData(string path, bool returnNull, StatsManager sm) {
-			if(!File.Exists(path)) {
-				if(!returnNull && sm == null) {
-					MainPlugin.logger.LogError("A null StatsManager was passed.");
-					return null;
+			try {
+				if(!File.Exists(path)) {
+					if(!returnNull && sm == null) {
+						MainPlugin.logger.LogError("A null StatsManager was passed.");
+						return null;
+					}
+					return returnNull ? null : new RankData(sm);
 				}
-				return returnNull ? null : new RankData(sm);
+				string data = File.ReadAllText(path);
+				var temp = JsonConvert.DeserializeObject<TempRankData>(data, settings);
+				NewSaveSystem.overrideForceNull = true;
+				var surrogate = new RankData(sm);
+				NewSaveSystem.overrideForceNull = false;
+				surrogate.ranks = temp.ranks;
+				surrogate.secretsAmount = temp.secretsAmount;
+				surrogate.secretsFound = temp.secretsFound;
+				surrogate.challenge = temp.challenge;
+				surrogate.levelNumber = temp.levelNumber;
+				surrogate.majorAssists = temp.majorAssists;
+				return surrogate;
+			} catch(Exception e) {
+				MainPlugin.logger.LogError(e);
+				MainPlugin.logger.LogError("An error occoured while loading saves and the game will now close.");
+				Application.Quit();
+				return new RankData(sm);
 			}
-			string data = File.ReadAllText(path);
-			var temp = JsonConvert.DeserializeObject<TempRankData>(data, settings);
-			NewSaveSystem.overrideForceNull = true;
-			var surrogate = new RankData(sm);
-			NewSaveSystem.overrideForceNull = false;
-			surrogate.ranks = temp.ranks;
-			surrogate.secretsAmount = temp.secretsAmount;
-			surrogate.secretsFound = temp.secretsFound;
-			surrogate.challenge = temp.challenge;
-			surrogate.levelNumber = temp.levelNumber;
-			surrogate.majorAssists = temp.majorAssists;
-			return surrogate;
 		}
 		public static T LoadData<T>(string path) where T : new() {
-			if(!File.Exists(path)) {
-				return new T();
+			try {
+				if(!File.Exists(path)) {
+					return new T();
+				}
+				string data = File.ReadAllText(path);
+				return JsonConvert.DeserializeObject<T>(data, settings);
+			} catch(Exception e) {
+				MainPlugin.logger.LogError(e);
+				MainPlugin.logger.LogError("An error occoured while loading saves and the game will now close.");
+				Application.Quit();
+				return default(T);
 			}
-			string data = File.ReadAllText(path);
-			return JsonConvert.DeserializeObject<T>(data, settings);
 		}
 		public static void SaveData<T>(string path, T data) {
-			settings.Formatting = SaveFileFormatting.Value;
-			string dataString = JsonConvert.SerializeObject(data, settings);
-			File.WriteAllText(path, dataString);
+			try {
+				settings.Formatting = SaveFileFormatting.Value;
+				string dataString = JsonConvert.SerializeObject(data, settings);
+				File.WriteAllText(path, dataString);
+			} catch(Exception e) {
+				MainPlugin.logger.LogError(e);
+				MainPlugin.logger.LogError("An error occoured while saving.");
+			}
 		}
 	}
 }
